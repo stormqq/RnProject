cba4f77f445c1cdb774607bf806f74c8160b380d
Anton Yarmak <perfect583@gmail.com>
2024-09-18 18:15:55 +0300

    upd

--- android/app/src/main/java/com/rnproject/textrecognition/textRecognitionPlugin.kt
+++ android/app/src/main/java/com/rnproject/textrecognition/textRecognitionPlugin.kt
@@ -1,39 +1,45 @@
 package com.rnproject.textrecognition
 
+import android.annotation.SuppressLint
+import android.graphics.Point
+import android.graphics.Rect
+import android.media.Image
+import androidx.camera.core.ImageProxy
+import com.facebook.react.bridge.WritableNativeArray
+import com.facebook.react.bridge.WritableNativeMap
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.TextRecognizer
-import com.mrousavy.camera.frameprocessors.Frame
-import com.mrousavy.camera.frameprocessors.FrameProcessorPlugin
-import com.mrousavy.camera.frameprocessors.VisionCameraProxy
-import java.nio.ByteBuffer
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin
 
 class TextRecognitionPlugin(proxy: VisionCameraProxy, options: Map<String, Any>?): FrameProcessorPlugin() {
 
-    private val recognizer: TextRecognizer = TextRecognition.getClient()
-
     override fun callback(frame: Frame, arguments: Map<String, Any>?): Any? {
-        val data = frame.data as ByteBuffer
-        val image = InputImage.fromByteBuffer(
-            data,
-            frame.width,
-            frame.height,
-            frame.rotation,
-            InputImage.IMAGE_FORMAT_NV21
-        )
 
-        val result = recognizer.process(image)
-            .addOnSuccessListener { visionText ->
-                visionText.textBlocks.forEach { block ->
-                    block.lines.forEach { line ->
-                        println(line.text)
-                    }
-                }
-            }
-            .addOnFailureListener { e ->
-                println("Text recognition failed: ${e.message}")
-            }
+        val result = WritableNativeMap()
 
-        return result
+        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+        val mediaImage: Image? = frame.getImage()
+
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, frame.imageInfo.rotationDegrees)
+            val task: Task<Text> = recognizer.process(image)
+            try {
+                val text: Text = Tasks.await<Text>(task)
+                result.putString("text", text.text)
+            } catch (e: Exception) {
+                return null
+            }
+        }
+
+        val data = WritableNativeMap()
+
+        data.putMap("result", result)
+
+        return data
     }
 }
